9d92dd19e486b147f7721a1c48354f0918ae842c
oussamaza25 <oussama.zahdi25@gmail.com>
2024-09-27 20:16:05 +0100

    minishell 6

--- parsing/ft_fill_token2.c
+++ parsing/ft_fill_token2.c
@@ -6,7 +6,7 @@
 /*   By: ozahdi <ozahdi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 12:20:23 by ozahdi            #+#    #+#             */
-/*   Updated: 2024/09/25 14:47:00 by ozahdi           ###   ########.fr       */
+/*   Updated: 2024/09/27 19:12:15 by ozahdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,11 +81,21 @@ static void	ft_in_out_pipe(t_data **line)
 static void	ft_builtings(t_data **line)
 {
 	t_token		*tmp;
+	char		*str;
 
 	(*line)->builtin_check = 0;
 	tmp = (*line)->pars_token;
 	while (tmp)
 	{
+		if (tmp->type == CMD && tmp->content[0] == 34)
+			str = ft_substr(tmp->content, 1, ft_strlen(tmp->content) - 2);
+		else if (tmp->type == CMD && tmp->content[0] == 39)
+			str = ft_substr(tmp->content, 1, ft_strlen(tmp->content) - 2);
+		else
+			str = ft_strdup(tmp->content);
+		free(tmp->content);
+		tmp->content = ft_strdup(str);
+		free(str);
 		if (tmp->type == CMD && (ft_compare(tmp->content, "echo") || \
 			ft_compare(tmp->content, "export") || \
 			ft_compare(tmp->content, "pwd") || \
